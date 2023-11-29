e7077bff4be33bcfc96ca6687ab639cfa68fb1b4
Heun11 <pacificko787@gmail.com>
2023-11-29 16:57:05 +0100

    male zmeny

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ var commands = {
     '  ███████║█████╗  ██║   ██║██╔██╗ ██║╚██║╚██║',
     '  ██╔══██║██╔══╝  ██║   ██║██║╚██╗██║ ██║ ██║',
     '  ██║  ██║███████╗╚██████╔╝██║ ╚████║ ██║ ██║',
-    '  ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═╝ ╚═ ',
+    '  ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═╝ ╚═╝',
     '&nbsp',
     ' Welcome to my portfolio web terminal.',
     ' For a list of available commands, type \'help\'.'
@@ -119,3 +119,16 @@ function getTextWidth(text, font)
   let metrics = context.measureText(text);
   return metrics.width;
 }
+
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    else{
+      entry.target.classList.remove('show');
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
